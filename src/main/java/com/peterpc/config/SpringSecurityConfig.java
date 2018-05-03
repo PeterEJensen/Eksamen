0b3642b714f0157997d40e133fe1f23043e4daa3
PeterLaptop <varaneh@hotmail.com>
2018-05-03 11:01:18 +0200

    Initial commit

---
+++ src/main/java/com/peterpc/config/SpringSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.peterpc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.sql.DataSource;
+
+@Configuration
+// http://docs.spring.io/spring-boot/docs/current/reference/html/howto-security.html
+// Switch off the Spring Boot security configuration
+//@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //@Qualifier("dataSource")
+    @Autowired
+    DataSource dataSource;
+    //@Autowired
+    private AccessDeniedHandler accessDeniedHandler;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/about", "/index", "/register", "/confirm").permitAll()
+                .antMatchers("/admin/**", "/delete", "/create", "/calendar", "/post").hasAnyRole("ADMIN")
+                .antMatchers("/user/**", "/calendar").hasAnyRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+    }
+
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select username,password, enabled from users where username=?")
+               // .usersByUsernameQuery("select email,enabled, password from user where email=?");
+                .authoritiesByUsernameQuery("select username, role from user_role where username=?");
+        //mangler Authories (foreign key til login med register
+    }
+
+}
---
+++ src/main/java/com/peterpc/config/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.peterpc.config;
+
+/*
+import com.peterpc.model.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    ArrayList<Student> students = new ArrayList<>();
+
+    public UserServiceImpl() {
+        students.add(new Student(0, "Peter", "Jensen", "12346578", new Date()));
+        students.add(new Student(1, "John", "Snow", "12346578", new Date()));
+        students.add(new Student(2, "Test person 3", "Generic last name", "12346578", new Date()));
+        students.add(new Student(3, "Viggo", "donut", "12346578", new Date()));
+        students.add(new Student(4, "test", "test", "12346578", new Date()));
+    }
+
+    @Override
+    public ArrayList<Student> fetchAllUsers() {
+        return students;
+    }
+
+}
+
+*/
